14aa01723c3e765fd4c95f01cd9cd18a4b38b444
Nasser Alkmim <nasser.alkmim@gmail.com>
2021-09-20 21:20:24 +0200

    Added option to choose serif font.

--- figtex.py
+++ figtex.py
@@ -5,11 +5,12 @@ def default():
     plt.style.use('default')
     return None
 
-def style():
+def style(serif='Computer Modern'):
     """ Use custom config."""
     config = {
         'text.usetex': True,    # use LaTeX to render text
         'font.family': 'serif',
+        'font.serif': [serif]
     }
     mpl.rcParams.update(config)
     return None
