fccfd7d7de2bb2a6be91ec68267f8adf4857c7a6
Nasser Alkmim <nasser.alkmim@gmail.com>
2022-04-01 11:35:27 +0200

    changed legend fontsize.

--- figtex.py
+++ figtex.py
@@ -1,19 +1,34 @@
+"""Nasser's default plot style."""
 import matplotlib.pyplot as plt
 
+
 def default():
+    """Make it matplotlib's default."""
     plt.style.use('default')
     return None
 
+
 def style(serif='Computer Modern'):
-    """ Use custom config."""
-    plt.style.use('seaborn-whitegrid')
+    """Set custom config."""
+    plt.style.use('seaborn-colorblind')
     config = {
         'text.usetex': True,    # use LaTeX to render text
         'font.family': 'serif',
         'font.serif': [serif],
-        'figure.constrained_layout.use': True,
+        # 'figure.constrained_layout.use': True,
         'legend.frameon': True,
         'legend.edgecolor': 'none',
+        'legend.fontsize': 8,
+        'font.size': 10.0,
+        'axes.grid': True,
+        'axes.linewidth': 1,    # seaborn changed to a ticker, better 1
+        'axes.edgecolor': '0.8',
+        'grid.color': '0.8',
+        'text.color': '0.15',
+        'xtick.color': '0.15',
+        'ytick.color': '0.15',
+        'lines.linewidth': 1,   # default is 1.5
+        'text.latex.preamble': r"\usepackage{amsmath}", # allows \text in math env
     }
     plt.rcParams.update(config)
     return None
